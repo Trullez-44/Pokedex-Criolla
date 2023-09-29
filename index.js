6adc928c37780e9719d6035f10db756216719ae6
Trullez-44 <136148374+Trullez-44@users.noreply.github.com>
2023-09-29 17:12:51 -0500

    feat: :sparkles: The 20 first pokemon load
    i did the feature that loads the 20 first pokemon

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+import {load, upload} from './modules/load.js'
+
+load(upload);
+
 let my_button = document.querySelector("#prueba");
 var root = document.documentElement;
 var miVariableCSS = getComputedStyle(root).getPropertyValue('--electric');
-console.log(miVariableCSS);
-const url = "https://pokeapi.co/api/v2/pokemon/";
+const url = "https://pokeapi.co/api/v2/pokemon";
 my_button.addEventListener("click", async () => {
-  let call = await (
-    await fetch(url + "169  " /*EL ENDPOINT ES EL NOMBRE O ID DEL POKEMON */)
-  ).json();
-  pokemon_image = call.sprites["front_default"];
-  imagen_suplente =
-    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/029b8bd9-cb5a-41e4-9c7e-ee516face9bb/dayo3ow-7ac86c31-8b2b-4810-89f2-e6134caf1f2d.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAyOWI4YmQ5LWNiNWEtNDFlNC05YzdlLWVlNTE2ZmFjZTliYlwvZGF5bzNvdy03YWM4NmMzMS04YjJiLTQ4MTAtODlmMi1lNjEzNGNhZjFmMmQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ooubhxjHp9PIMhVxvCFHziI6pxDAS8glXPWenUeomWs";
+  
+  let call = await (await fetch(url+"/9"/*EL ENDPOINT ES EL NOMBRE O ID DEL POKEMON */)).json();
+  let pokemon_image = call.sprites["front_default"];
+  let imagen_suplente ="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/029b8bd9-cb5a-41e4-9c7e-ee516face9bb/dayo3ow-7ac86c31-8b2b-4810-89f2-e6134caf1f2d.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzAyOWI4YmQ5LWNiNWEtNDFlNC05YzdlLWVlNTE2ZmFjZTliYlwvZGF5bzNvdy03YWM4NmMzMS04YjJiLTQ4MTAtODlmMi1lNjEzNGNhZjFmMmQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.ooubhxjHp9PIMhVxvCFHziI6pxDAS8glXPWenUeomWs";
   // console.log(call.stats[0].stat.name);
   Swal.fire({
     title: `
@@ -37,9 +38,9 @@ my_button.addEventListener("click", async () => {
         <input type="submit" value="SEND INFO" form="div_">`,
     imageAlt: "Pokemon Image",
   });
-  let container = document.querySelector("#div_");
-  container.addEventListener("input", (e)=>{
-    if(e.target.matches("input")){
+  let container = document.querySelector("#cards");
+  container.addEventListener("click", (e)=>{
+    if(e.target.matches("click")){
         let mystats =(e.target.parentNode.children[0]);
         mystats.innerHTML = ` ${e.target.value} ${mystats.dataset.name.toUpperCase()}`
     }
---
+++ modules/load.js
@@ -0,0 +1,33 @@
+let url = "https://pokeapi.co/api/v2/pokemon"
+let div_cards = document.getElementById('listaPokemon')
+const insertHTML =(img, name, id)=>{
+        return  `
+            <div class="pokemon">
+                <p class="pokemon-id-back">${id}</p>
+                <div class="pokemon-imagen">
+                    <img src=${img}
+                        alt="Pikachu">
+                </div>
+                <div class="pokemon-info">
+                    <div class="nombre-contenedor">
+                        <p class="pokemon-id">${id}</p>
+                        <h2 class="pokemon-nombre">${name}</h2>
+                    </div>
+                </div>
+            </div>
+        `
+}
+export const upload = async(res_data)=>{
+    res_data.results.forEach(async element=> {
+        let e = await (await fetch(element.url)).json();
+        // console.log(e.sprites.front_default, e.name, e.id);
+        let card = insertHTML(e.sprites.front_default, e.name, e.id);
+
+        div_cards.innerHTML +=card
+    });
+}
+export const load =async (upload)=>{
+    let res= await (await fetch(url)).json();
+    upload(res);
+}
+    
